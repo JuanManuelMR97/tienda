8731fa543d9270c869b6f7e6e5aa864c5df31f63
Juan Manuel <juanmasbt97@gmail.com>
2019-02-21 10:55:04 +0100

    Documento funciones

--- application/helpers/funciones_helper.php
+++ application/helpers/funciones_helper.php
@@ -2,6 +2,13 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * Devuelve un array asociativo con el formato clave => valor
+ * @param array $rs
+ * @param string $clave
+ * @param string $valor
+ * @return array
+ */
 function dbresult_to_array($rs, $clave, $valor) {
     $res = [];
     foreach ($rs as $reg) {
@@ -10,6 +17,15 @@ function dbresult_to_array($rs, $clave, $valor) {
     return $res;
 }
 
+/**
+ * Devuelve un array asociativo con el formato clave => valor, permitiendo 
+ * introducir por parámetro el primer valor del mismo con una clave nula por defecto
+ * @param array $rs
+ * @param string $clave
+ * @param string $valor
+ * @param string $default
+ * @return array
+ */
 function dbresult_to_array_null($rs, $clave, $valor, $default = '') {
     $res = ['' => $default];
     foreach ($rs as $reg) {
@@ -18,6 +34,10 @@ function dbresult_to_array_null($rs, $clave, $valor, $default = '') {
     return $res;
 }
 
+/**
+ * Devuelve todas las gategorías existentes
+ * @return array
+ */
 function get_categorias() {
     $ci = get_instance();
     $ci->db->select('id_categoria, nombre');
@@ -25,6 +45,10 @@ function get_categorias() {
     return $query->result();
 }
 
+/**
+ * Devuelve todas las provincias
+ * @return array
+ */
 function get_provincias() {
     $ci = get_instance();
     $ci->db->select('cod, nombre');
